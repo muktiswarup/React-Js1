660d7b169fd688540944e1fbbf215d9fc67a2f61
muktiswarup <csjena1999@gmail.com>
2024-08-22 20:41:55 +0530

    'j'

--- RegistrationForm/src/App.jsx
+++ RegistrationForm/src/App.jsx
@@ -1,29 +1,127 @@
-import { useState } from "react"
-
+/* 
+import { useState } from "react";
 
 function App() {
-  const data={name:'',email:'',pwd:''}
- const [feild,setFeild]=useState(data);
- const handleInput=(e)=>{
-    setFeild({...feild,[e.target.name]:e.target.value})
+  const data = { name: "", email: "", pwd: "" };
+  const [feild, setFeild] = useState(data);
+
+  const handleChange = (e) => {
+    setFeild({ ...feild, [e.target.name]: e.target.value });
     console.log(feild);
-    console.log(e.target.name)
- }
+    console.log(e.target.name);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(feild);
+  };
 
   return (
     <>
-      <form style={{display:"grid",justifyContent:'center', marginLeft:'100%',width:'100%',alignContent:'center'}}>
-        <div style={{display:"grid",justifyContent:'center', marginLeft:'50%',width:'50%',alignContent:'center'}}>
-        <input type="text"  placeholder="Enter Your Name" required value={feild.name} onChange={handleInput} style={{alignContent:'center'}} name='name'/><br />
-        <input type="email"placeholder="Enter Your Email" required value={feild.email}  onChange={handleInput} email='email'/><br />
-        <input type="number"  placeholder="Enter Your password" required value={feild.pwd}  onChange={handleInput} password='pwd'/><br />
-        <button>Submit</button>
+      <form
+        style={{ display: "grid", justifyContent: "center", width: "100%",  marginTop: "5rem"}}
+        onSubmit={submitHandler}
+      >
+        <div
+          style={{ display: "grid", justifyContent: "center", width: "50%" }}
+        >
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            required
+            value={feild.name}
+            onChange={handleChange}
+            name="name"
+            style={{ marginBottom: "10px" }}
+          />
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            required
+            value={feild.email}
+            onChange={handleChange}
+            name="email"
+            style={{ marginBottom: "10px" }}
+          />
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            required
+            value={feild.pwd}
+            onChange={handleChange}
+            name="pwd"
+            style={{ marginBottom: "10px" }}
+          />
+          <input type="submit" value="Submit" style={{ cursor: "pointer" }} />
+          
         </div>
-       
-
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App; */
+
+
+import { useState } from "react";
+
+function App() {
+  const data = { name: "", email: "", pwd: "" };
+  const [feild, setFeild] = useState(data);
+
+  const handleChange = (e) => {
+    setFeild({ ...feild, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(feild);
+    // Optionally reset the form fields after submission
+    setFeild(data);
+  };
+
+  return (
+  
+    <>
+      <form
+        style={{ display: "grid", justifyContent: "center", width: "100%", marginTop: "10rem " }}
+        onSubmit={submitHandler}
+      >
+        <div
+          style={{ display: "grid", justifyContent: "center", width: "50%" }}
+        >
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            required
+            value={feild.name}
+            onChange={handleChange}
+            name="name"
+            style={{ marginBottom: "10px", padding:'10px' }}
+          />
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            required
+            value={feild.email}
+            onChange={handleChange}
+            name="email"
+            style={{ marginBottom: "10px",padding:'10px' }}
+          />
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            required
+            value={feild.pwd}
+            onChange={handleChange}
+            name="pwd"
+            style={{ marginBottom: "10px", padding:'10px' }}
+          />
+          <input type="submit" value="Submit" style={{ cursor: "pointer", background:'green', padding:'5px' }} />
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
