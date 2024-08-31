7f7143c982a94773aef33518856d11bf02a947d4
muktiswarup <csjena1999@gmail.com>
2024-09-01 00:20:58 +0530

    h

--- Bootstrap/src/Component/Product.jsx
+++ Bootstrap/src/Component/Product.jsx
@@ -1,42 +1,59 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Product = () => {
-    const [product,setProuct]=useState([]);
-    useEffect(()=>{
-        const fetchProduct= async ()=>{
-            try {
-                const response= await axios.get('https://fakestoreapi.com/products')
-                setProuct(response.data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchProduct();
-    })
-  return (<>
-        <div className="container-fluid mt-5">
-            {product.length===0}
-            <div className="row">
-                <div className="col-3">
-                    <div className="card">
-                        <div className="card-header">
-                            <img src="https://cubanvr.com/wp-content/uploads/2023/07/ai-image-generators.webp" alt="Something" width='100%' />
-                            <h4>Title</h4>
-                        </div>
-                        <div className="card-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <p>Category</p>
-                            <h5>Price</h5>
-                            <h6>rating</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-  </>
-    
-  )
-}
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState(null);
 
-export default Product
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProduct(response.data);
+      } catch (error) {
+        setError(error);
+        console.error("There is an error:", error);
+      }
+    };
+    fetchProduct();
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid mt-5">
+        {error && <p>There is an error: {error.message}</p>}
+        {product.length === 0 ? (
+          <p>Loading the Data...</p>
+        ) : (
+          <div className="row row-cols-1 row-cols-md-4 g-4">
+            {product.map((product, index) => {
+              return (
+                <div className="col" key={index}>
+                  <div className="card h-100">
+                    <div className="card-header text-center">
+                      <img
+                        src={product.image}
+                        alt={product.title}
+                        className="product-img"
+                        width='50%'
+                      />
+                      <h4>{product.title}</h4>
+                    </div>
+                    <div className="card-body text-center">
+                      <p>Category: {product.category}</p>
+                      <h5>Price: ${product.price}</h5>
+                      <h6>Rating: {product.rating.rate}</h6>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Product;
+
